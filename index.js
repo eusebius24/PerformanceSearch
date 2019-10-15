3953780d7938c10df6e6e6569fbe475b43c05625
Erika Reiman <erika.reiman@gmail.com>
2019-10-15 09:34:45 -0400

    Added youtube search function

--- index.js
+++ index.js
@@ -1,67 +1,72 @@
 'use strict';
-
+var youtubeAPIKey = "AIzaSyDS8Gv9yL6qBNOmZQvC2Hg-d30QcAv4cLo";
 
 function watchForm() {
-    $('form').submit(event =>{
+    $('form').submit(event => {
         event.preventDefault();
         console.log('you clicked the search button!');
         var searchTerm = $('#searchTerm').val();
         console.log(searchTerm);
-        var searchWords = [];
-        searchWords = searchTerm.split(' ');
-       
-        
-        getSearchResults(searchWords);
+        // var searchWords = [];
+        // searchWords = searchTerm.split(' ');
+        // var regexFromSearchWords = new RegExp(searchWords.join("|", "gi"));
+        // console.log(regexFromSearchWords);
+        getSearchResults(searchTerm);
     });
 }
 
-function getSearchResults(array) {
-        for (var j=0; j<150000; j+=1000) {
-            var imslpURL = "http://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/sort=id/type=2/start="+ [j] + "/retformat=json";
-            var queryURL = "https://cors-anywhere.herokuapp.com/" + imslpURL;
-            fetch(queryURL).then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error(response.statusText);
-                })
-                .then(function(responseJson) {
-                    for(let i in responseJson) {
-                        var composer = responseJson[i].intvals.composer;
-                        var work = responseJson[i].intvals.worktitle;
-                        var permalink = responseJson[i].permlink;
-                        var composerLowerCase = composer.toLowerCase();
-                        var workLowerCase = work.toLowerCase();
-                        
-                        var searchResults = [];
-                        for (var k=0; k < array.length; k++) {
-                            
-                            var term = array[k];
-                            
-                            var termLowerCase = term.toLowerCase();
-                        if (composerLowerCase.includes(termLowerCase) || workLowerCase.includes(termLowerCase)) {
-                            var workInfo = {"composer": composer,
-                                            "work": work
-                        };
-                            searchResults.push(workInfo);
-                            
-                            console.log(composer, work);
-                            var resultString = `<li>Composer: ${composer}<br>Work: ${work}<br><a href='${permalink}'>Permalink to score page</a></li>`;
-                            console.log(resultString);
-                            $('.imslp-results').append(resultString);
-                        }
-                    
-                        
-                    }
-                }
-
-                })
-    
+function getSearchResults(term) {
+    var termArray = term.split(' ');
+    var newTerm = termArray.join('%20');
+    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q='" + newTerm + "'&key=" + youtubeAPIKey;
+    console.log(url);
+    fetch(url)
+        .then(response => {
+        if (response.ok) {
+            return response.json();
         }
-    
+        throw new Error(response.statusText);
+        })
+        // .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        })
     $('.results').removeClass('hidden');
 }
 
+function displayResults(responseJson) {
+    for (var i in responseJson.items) {
+    console.log(responseJson.items[i].snippet.title);
+    $('.youtube-results').append(`<li><img src='${responseJson.items[i].snippet.thumbnails.medium.url}'><br><a href='http://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}'>${responseJson.items[i].snippet.title}</a></li>`);
+    }
+    getMoreYouTubeResults(responseJson);
+    
+}
+
+function getMoreYouTubeResults(responseJson) {
+    $('#more-youtube-results').click(function () {
+        event.preventDefault();
+        var searchTerm = $('#searchTerm').val();
+        var termArray = searchTerm.split(' ');
+        var newTerm = termArray.join('%20');
+        var moreResultsUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=${newTerm}&key=${youtubeAPIKey}&pageToken=${responseJson.nextPageToken}`
+        $('.youtube-results').empty();
+        fetch(moreResultsUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+            })
+            // .then(responseJson => console.log(responseJson))
+            .then(responseJson => displayResults(responseJson))
+            .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            })
+    });
+    console.log(responseJson.nextPageToken);
+}
 $(function() {
     console.log('App loaded! Waiting for submit!');
     watchForm();
